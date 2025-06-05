2dbb48928512dc7e4a6fe473bdc0fd1d73ca52d3
Francelle Sarayan <lorrainesarayan@gmail.com>
2025-06-05 18:38:02 +0800

    bye

--- todo-list.js
+++ todo-list.js
@@ -1,8 +1,15 @@
 
 
 const SignInButton = document.getElementById("Sign-In-Button");
+const Homepage = document.getElementById("HomePage");
+const LoginPage = document.querySelector(".Main-Container"); // main container
+
 
 SignInButton.addEventListener("click", function() {
+  LoginPage.style.display = "none";
+  Homepage.style.display = "flex";
+});
 
-})
+
+
 
