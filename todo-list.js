428fc9fa8a9ea0ff313d14b616a8f85a685e9a43
Francelle Sarayan <lorrainesarayan@gmail.com>
2025-06-06 01:35:14 +0800

    side dashboard design slightly arraso T_T

--- todo-list.js
+++ todo-list.js
@@ -1,13 +1,13 @@
 
 
 const SignInButton = document.getElementById("Sign-In-Button");
-const Homepage = document.getElementById("HomePage");
-const LoginPage = document.querySelector(".Main-Container"); // main container
+const HomePage = document.getElementById("HomePage");
+const SignIn_Page = document.querySelector(".Main-Container"); // main container
 
 
 SignInButton.addEventListener("click", function() {
-  LoginPage.style.display = "none";
-  Homepage.style.display = "flex";
+  SignIn_Page.style.display = "none";
+  HomePage.style.display = "flex";
 });
 
 
